00a87294c43775472d4d2b75868c59441d8434ad
Daniel Grabert <dg@indivirtuell.net>
2014-11-21 03:15:17 +0100

    Mocking of pi-gpio added to unit tests

--- test/rasp-gpio_test.js
+++ test/rasp-gpio_test.js
@@ -1,10 +1,28 @@
 /*global describe,it*/
 'use strict';
-var assert = require('assert'),
-  raspGpio = require('../lib/rasp-gpio.js');
+
+// var assert = require('assert');
+var expectRequire = require('a').expectRequire;
+var sinon = require('sinon');
+
+var fakePiGpio = {
+	open: sinon.stub().callsArgWith(2),
+	close: sinon.stub(),
+	write: sinon.stub().callsArgWith(2)
+};
+
+expectRequire('pi-gpio').return(fakePiGpio);
+
+var  raspGpio = require('../lib/rasp-gpio.js');
 
 describe('rasp-gpio node module.', function() {
-  it('must be awesome', function() {
-    assert( raspGpio.awesome(), 'awesome');
+  it('should do something', function() {
+    	raspGpio.run();
+    	sinon.assert.called(fakePiGpio.open);
+    	sinon.assert.called(fakePiGpio.close);
+    	sinon.assert.called(fakePiGpio.write);
   });
 });
+
+
+
