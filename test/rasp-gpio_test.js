e1b6c9a11fb0c1c0d42294de971f48b81e313b62
Daniel Grabert <dg@indivirtuell.net>
2014-11-21 04:19:52 +0100

    Refactoring, tests updated

--- test/rasp-gpio_test.js
+++ test/rasp-gpio_test.js
@@ -1,10 +1,11 @@
 /*global describe,it*/
 'use strict';
 
-// var assert = require('assert');
-var expectRequire = require('a').expectRequire,
+var assert = require('chai').assert,
+	expectRequire = require('a').expectRequire,
 	sinon = require('sinon');
 
+// Mocked pi-gpio module
 var fakePiGpio = {
 	open: sinon.stub().callsArgWith(2),
 	close: sinon.stub(),
@@ -16,6 +17,28 @@ expectRequire('pi-gpio').return(fakePiGpio);
 var raspGpio = require('../lib/rasp-gpio.js');
 
 describe('rasp-gpio node module.', function() {
+
+	describe('PINs', function () {
+		it('should provide an object pins', function () {
+			assert.ok(raspGpio.pins);
+		});
+		it('should have pins defined', function () {
+			assert.deepEqual(raspGpio.pins, { LED: 12 });
+		});
+	});
+
+
+	describe('methods', function () {
+		it('should provide a method _turnOn', function () {
+			assert.ok(raspGpio._turnOn);
+		});
+
+		it('should provide a method _turnOff', function () {
+			assert.ok(raspGpio._turnOff);
+		});
+	});
+
+
   it('should do something', function() {
     	raspGpio.run();
     	sinon.assert.called(fakePiGpio.open);
